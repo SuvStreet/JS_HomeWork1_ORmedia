74fe4241125adf1af4b3197023eeed91ed292840
Дмитрий Колотуша <suvstreet@gmail.com>
2020-05-23 16:27:49 +0300

    complate task1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var block = document.getElementById('div');
+var block = document.getElementById('div_style');
 
 function editColorBlock(){
     if(block.style.backgroundColor === 'green'){
@@ -6,5 +6,20 @@ function editColorBlock(){
     }
     else {
         block.style.backgroundColor = 'green';
+    }
+};
+
+var show = document.getElementById('div_style');
+
+function addBlock(){
+    if(show.style.display === 'block'){
+        show.style.display = 'none';
+        document.getElementById('div1').style.alignItems = 'flex-end';
+        document.getElementById('div2').style.display = 'block';
+    }
+    else {
+        show.style.display = 'block';
+        document.getElementById('div2').style.display = 'none';
+        document.getElementById('div1').style.alignItems = 'center';
     }
 };
